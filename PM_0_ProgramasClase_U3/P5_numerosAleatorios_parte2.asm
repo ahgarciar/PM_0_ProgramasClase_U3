7ad0bac0adb628e1a1a4295d2432661c1c3cea84
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2021-10-20 08:56:21 -0500

    Initial commit

---
+++ PM_0_ProgramasClase_U3/P5_numerosAleatorios_parte2.asm
@@ -0,0 +1,53 @@
+	TITLE NombrePrograma
+
+;DESCRPICIÓN 
+;Objetivo: Archivo de Ejemplo
+;
+; Autore(s):
+;         Apellido Nombre Integrante 1
+;         Apellido Nombre Integrante 2
+
+; Semestre: 8vo Semestre ISC 
+;FIN DESCRPICIÓN
+
+INCLUDE Irvine32.inc  
+
+.data
+; Área de Declaración de Variables
+
+
+.code
+
+	main5 PROC
+	
+		;Lógica del Programa
+
+		mov ecx, 10
+
+		call randomize   ; establece la semilla para el generador de numeros aleatorios con base en la hora dél dia en que se ejecuta
+					     ; Su funcionalidad no puede ser visualizada desde consola 
+						 ; Se recomienda utilizarlo únicamente 1 vez por programa, al iniciar este
+		
+		ciclo:
+	
+		mov eax, 101   ; establece la semilla para generar numeros aleatorios entre 0 y 100 (no llega al 101)
+	
+
+		call writeint    ; imprime el valor de eax
+
+		call randomrange   ; genera un numero aleatorio entre 0 y el valor de eax (exclusive). el numero generado lo almacena en eax
+						   ; En este sentido, si despues se desea generar otro numero aleatorio en el mismo intervalo, entonces
+						   ; se debe colocar nuevamente al valor del limite en eax.
+
+		call writeint    ; imprime el valor de eax (que en este punto corresponde al numero aleatorio obtenido)
+
+		call crlf
+
+		loop ciclo
+
+		
+		exit	
+	
+	main5 ENDP
+	
+	END main5
